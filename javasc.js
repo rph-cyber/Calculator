45a77e94b96ea2305dd91152552d3d09c731f94f
rph-cyber <119410947+rph-cyber@users.noreply.github.com>
2023-03-17 13:23:21 +0200

    Added js

---
+++ javasc.js
@@ -0,0 +1,51 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    // Store all components on HTML in our JS
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+})
+
+function handleNumber(num){
+    if(currentValue.length <= 5){
+    currentValue += num;
+    }
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
